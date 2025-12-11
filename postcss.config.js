b99fc2b6525202804729e60fde59ff1528f4b4d3
Lavanya Sharma <127662996+lava918@users.noreply.github.com>
2025-12-11 18:13:34 +0530

    Updating files

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // Change 'tailwindcss' to '@tailwindcss/postcss'
     autoprefixer: {},
   },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'; //
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
+      {/* Navbar would go here */}
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        {/* Add other routes like /collection, /about, /contact, /product/:productId, etc. */}
+      </Routes>
+    </div>
   )
 }
 
